06343184c08de59b1a8b9a34cb7a68ba00d92536
adrianRTA <adrian62504@gmail.com>
2026-05-04 22:19:30 -0700

    add counter, needs bugfixing

---
+++ project_folder/scripts/counter.js
@@ -0,0 +1,11 @@
+// I've been here function
+
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    countLabel.textContent = count;
+}
